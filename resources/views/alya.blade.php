ec9608a42d25ea5e3fdf7deaf69b5adf040a1244
alyatulmasruroh <alyatulyanti75@smk.belajar.id>
2022-05-24 11:46:40 +0700

    24 mei 2022

---
+++ resources/views/alya.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>alya</title>
+</head>
+<body>
+    <h1>Alyatul Masruroh Widya Yanti XI-RPL1</h1>
+</body>
+</html>
